ba8f79e8a9bc925fb798827133b7b5dea0d17d8e
Damir Kusar <damir@kusar.ch>
2015-03-16 18:56:40 +0100

    removed not needed files

--- public/application.config.js
+++ public/application.config.js
@@ -6,14 +6,3 @@ CssConfiguration.registerModule("bower_components/bootstrap/dist/css/bootstrap.m
 JsConfiguration.registerModule("app/app.module.js");
 JsConfiguration.registerModule("modules/welcome/welcome.module.js");
 JsConfiguration.registerModule("modules/page1/page1.module.js");
-
-/*JsConfiguration.registerModule("bower_components/jquery/jquery.js");
- JsConfiguration.registerModule("bower_components/bootstrap/dist/js/bootstrap.js");
- JsConfiguration.registerModule("bower_components/underscore/underscore.js");
- JsConfiguration.registerModule("bower_components/moment/moment.js");
- JsConfiguration.registerModule("bower_components/angular/angular.js");
- JsConfiguration.registerModule("bower_components/angular-ui-router/release/angular-ui-router.js");
- JsConfiguration.registerModule("bower_components/angular-animate/angular-animate.js");
- JsConfiguration.registerModule("bower_components/angular-resource/angular-resource.js");
- JsConfiguration.registerModule("bower_components/angular-bootstrap/ui-bootstrap-tpls.js");
- JsConfiguration.registerModule("bower_components/angular-ui-utils/ui-utils.js");*/
