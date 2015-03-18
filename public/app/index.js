24988f7fb771a3dc99657af9f5c57d3f4f5ba938
Damir Kusar <damir@kusar.ch>
2015-03-18 09:15:07 +0100

    renamed module classes to index.js to meet browserify req

--- public/app/index.js
+++ public/app/index.js
@@ -3,5 +3,5 @@
 JsConfiguration.registerModule("app/js/app.controller.js");
 JsConfiguration.registerModule("app/js/services/scrollToElement.client.service.js");
 JsConfiguration.registerModule("app/js/services/menus.client.service.js");
-JsConfiguration.registerModule("app/partials/footer/footer.module.js");
-JsConfiguration.registerModule("app/partials/nav/nav.module.js");
+JsConfiguration.registerModule("app/partials/footer/index.js");
+JsConfiguration.registerModule("app/partials/nav/index.js");
--- public/application.config.js
+++ public/application.config.js
@@ -3,6 +3,6 @@
 CssConfiguration.registerModule("app.css");
 CssConfiguration.registerModule("bower_components/bootstrap/dist/css/bootstrap.min.css");
 
-JsConfiguration.registerModule("app/app.module.js");
-JsConfiguration.registerModule("modules/welcome/welcome.module.js");
-JsConfiguration.registerModule("modules/page1/page1.module.js");
+JsConfiguration.registerModule("app/index.js");
+JsConfiguration.registerModule("modules/welcome/index.js");
+JsConfiguration.registerModule("modules/page1/index.js");
