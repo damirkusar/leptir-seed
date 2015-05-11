718e7278ae0c6e33395d2aaaa5f3f4224906d353
Damir Kusar <damir@kusar.ch>
2015-05-11 11:33:11 +0200

    changed menu

--- public/modules/demo/js/config/demo.menu.js
+++ public/modules/demo/js/config/demo.menu.js
@@ -3,6 +3,6 @@
 angular.module('demo').run(['Menus',
     function(Menus) {
         // Set top bar menu items
-        Menus.addMenuItem('topBar', 'demo1Id', 'Demo', 'demo');
+        Menus.addMenuItem('topBar', 'demo1Id', 'Demo', 'demo', '', '');
     }
 ]);
--- public/modules/welcome/js/config/welcome.menu.js
+++ public/modules/welcome/js/config/welcome.menu.js
@@ -3,8 +3,8 @@
 angular.module('welcome').run(['Menus',
     function(Menus) {
         // Set top bar menu items
-        Menus.addMenuItem('topBar', 'welcomeId', 'Welcome', '', 'dropdown');
-        Menus.addSubMenuItem('topBar', 'welcomeId','sub1Id' ,'To Welcome Test', 'welcome.test');
-        Menus.addSubMenuItem('topBar', 'welcomeId', 'sub2Id', 'To Demo', 'demo');
+        Menus.addMenuItem('topBar', 'welcomeId', 'Welcome', '', 'dropDown', '');
+        Menus.addSubMenuItem('topBar', 'welcomeId','sub1Id' ,'To Welcome Test', 'welcome.test', '');
+        Menus.addSubMenuItem('topBar', 'welcomeId', 'sub2Id', 'To Demo', 'demo', '');
     }
 ]);
